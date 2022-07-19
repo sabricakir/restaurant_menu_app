20d3013f8b424c7161ead2a3260878d0cefa5157
sabricakir <sabricakir86@gmail.com>
2022-07-19 17:41:01 +0300

    updated about page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_19_110110) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_19_134633) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
