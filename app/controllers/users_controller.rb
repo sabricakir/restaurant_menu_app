2c58e12d6787701fb33fb5420440ad09bcd72c4a
rizadogukankosar <rizadogukankosar@gmail.com>
2022-08-02 10:39:26 +0300

    redirect's and categories

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@ class UsersController < ApplicationController
 
   def show
 		@user = User.find(params[:id])
-		if current_user.id != @user.id
-			#function
+		if current_user==nil || current_user.id != @user.id
+			redirect_to root_path
 	   end
    end
 
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,8 @@
 class Category < ApplicationRecord
 
+  has_one_attached :category_icon
+
+
   validates :name, presence: {message:"İsim alanı zorunludur"}, length: {in: 2..40 , too_long: "İsim en fazla %{count} karakterden oluşabilir", too_short: "İsim alanı en az 2 karakterden oluşmalıdır" }
 
 end
